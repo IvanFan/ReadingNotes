8baad9dca23427580683f1b931bc6f59cf979d07
Ivan Fan <fanyfivan@gmail.com>
2020-12-02 00:13:36 +1100

    went through until page 90

--- cpp_primer_5/chap_1/1_7.cpp
+++ cpp_primer_5/chap_1/1_7.cpp
@@ -3,3 +3,19 @@
 //
 //class
 
+struct Sales_data{
+    std::string isbn() const { return bookNo;} // return this->bookNo const is used for this
+};
+
+// it is similar to
+std:: string Sales_data::isbn(const Sales_data *const this) { return this->bookNo; }
+
+// return *this; is a trick
+
+//cpp 11
+Sales_data() = default; //default constructor fun
+
+Sales_data(const std::string &s, unsigned n, double p): bookNo(s), units_sold(n), revenue(p*n) {}; // init bookNo, units_sold and revenue with input params
+
+
+
